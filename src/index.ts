ae2d114d1631bb9cca0da6d661078a9cfa67294b
Pedro Enciso <penciso@ucsd.edu>
2024-09-09 17:20:24 -0700

    renamed __dirname to avoid conflicts

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,14 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import path from "path";
 
-const __dirname = path.resolve();
+const dirname = path.resolve();
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
 // set pug as the view engine
-app.set("views", path.join(__dirname, "src", "views"));
+app.set("views", path.join(dirname, "src", "views"));
 app.set("view engine", "pug");
 
 app.get("/bot/jobs", (req: Request, res: Response) => {
