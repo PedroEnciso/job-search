0eb8001b363269cf3a67ae48ea8007318d585633
Pedro Enciso <penciso@ucsd.edu>
2024-09-11 17:37:39 -0700

    created queries to get companies and create a batch request

---
+++ src/db/queries.ts
@@ -0,0 +1,14 @@
+import { db } from ".";
+import { batchRequestTable } from "./schema";
+import type { Company } from "../types";
+
+export async function getCompanyUrls(): Promise<Array<Company>> {
+  return await db.query.companyTable.findMany();
+}
+
+export async function createBatchRequest(request_id: string): Promise<void> {
+  await db.insert(batchRequestTable).values({
+    id: request_id,
+    status: "validating",
+  });
+}
