de14eaf68154dadfb9cf1c4419dd6a806f431986
Pedro Enciso <penciso@ucsd.edu>
2024-09-12 10:06:39 -0700

    removed dist from git

--- dist/src/validIP.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ipAddresses = void 0;
-exports.ipAddresses = [
-    "116.203.129.16",
-    "116.203.134.67",
-    "23.88.105.37",
-    "128.140.8.200",
-];
