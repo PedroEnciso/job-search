bfaa84b8e331dc9d815a36d10e1a7cb97a8c4ddf
Pedro Enciso <penciso@ucsd.edu>
2024-09-16 16:43:04 -0700

    partial get /batchResponse logic

--- src/routes/bot.ts
+++ src/routes/bot.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from "express";
-import { ipAddresses } from "../validIP";
-import { getCompanyUrls, createBatchRequest } from "../db/queries";
+import {
+  getCompanyUrls,
+  createBatchRequest,
+  getOldestPendingBatchRequest,
+} from "../db/queries";
 import scraperAPI from "../lib/scraper";
 import fileWriterAPI from "../lib/fileWriter";
 import openaiAPI from "../lib/openai";
@@ -9,10 +12,8 @@ import type { Company } from "../types";
 export const botRouter = express.Router();
 
 botRouter.get("/jobs", async (req: Request, res: Response) => {
-  const initialTime = Date.now();
-  // validate that the user's ip is from cron-job.org
-  const user_ip = req.socket.remoteAddress;
-  const ip_is_valid = ipAddresses.filter((ip) => ip === user_ip).length === 1;
+  // validate that the request contains the auth code
+  const auth_code = req.headers.Auth;
   // IIFE to return response immediately
   (async () => {
     // get all companys
@@ -28,17 +29,37 @@ botRouter.get("/jobs", async (req: Request, res: Response) => {
     // create batch request using file id
     const batch_request = await openaiAPI.createBatchRequest(file.id);
     // save batch id to db
-    await createBatchRequest(batch_request.id);
+    await createBatchRequest(batch_request.id, file.id);
   })();
 
-  res.send(
-    `Request was sent from ${user_ip} which ${
-      ip_is_valid ? "is" : "is not"
-    } a valid address.`
-  );
+  res.send(`Request cantained auth code: ${auth_code}`);
 });
 
-botRouter.get("/batchResponse", (req: Request, res: Response) => {
+botRouter.get("/batchResponse", async (req: Request, res: Response) => {
+  // IIFE to return response immediately
+  (async () => {
+    // get the oldest batch request where status is not equal to failed, canceled, expired or completed
+    const db_batch_request_array = await getOldestPendingBatchRequest();
+    // ensure that a request was found
+    if (db_batch_request_array.length > 0) {
+      const db_batch_request = db_batch_request_array[0];
+      // fetch the batch request from openai
+      const batch_request = await openaiAPI.getBatchRequest(
+        db_batch_request.id
+      );
+      // check if the status has changed
+      if (batch_request && batch_request.status !== db_batch_request.status) {
+        // update the status in the db
+      }
+      // check if the status is completed
+      if (batch_request.status === "completed") {
+        // fetch the file response from openai
+        // const fileResponse = await openai.files.content(batch_request.file_id);
+        // const fileContents = await fileResponse.text();
+        // TODO: logic for retreiving fileContents and writing to db
+      }
+    }
+  })();
   res.send("TODO: Check for batch response.");
 });
 
