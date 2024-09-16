030e9fb7123a17416415819441aeff135a38573b
Pedro Enciso <penciso@ucsd.edu>
2024-09-16 16:38:21 -0700

    added getOldestPendingBatchRequest

--- src/db/queries.ts
+++ src/db/queries.ts
@@ -1,9 +1,31 @@
 import { db } from ".";
+import { notInArray } from "drizzle-orm";
 import { batchRequestTable } from "./schema";
-import type { Company } from "../types";
+import type { Company, BatchRequest } from "../types";
 
+// SELECT queries
+// TODO: rename function to getAllCompanys
 export async function getCompanyUrls(): Promise<Array<Company>> {
   return await db.query.companyTable.findMany();
+}
+
+// returns the oldest pending batch request based on created_at column
+export async function getOldestPendingBatchRequest(): Promise<
+  Array<BatchRequest>
+> {
+  return await db
+    .select()
+    .from(batchRequestTable)
+    .where(
+      notInArray(batchRequestTable.status, [
+        "failed",
+        "cancelled",
+        "expired",
+        "completed",
+      ])
+    )
+    .orderBy(batchRequestTable.created_at)
+    .limit(1);
 }
 
 export async function createBatchRequest(
