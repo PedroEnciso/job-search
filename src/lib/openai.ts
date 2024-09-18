c1ca5f6fe141497711c652d5d9cbc9c8a2ceeae1
Pedro Enciso <penciso@ucsd.edu>
2024-09-17 18:05:33 -0700

    error handling and prompt update

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,32 +1,49 @@
 import OpenAI from "openai";
 import fs from "fs";
+import { getErrorMessage } from "./util";
 const openai = new OpenAI();
 const openai_model = "gpt-4o-mini";
 
 const openaiAPI = {
   async createBatchRequest(file_id: string) {
-    return await openai.batches.create({
-      input_file_id: file_id,
-      endpoint: "/v1/chat/completions",
-      completion_window: "24h",
-    });
+    try {
+      return await openai.batches.create({
+        input_file_id: file_id,
+        endpoint: "/v1/chat/completions",
+        completion_window: "24h",
+      });
+    } catch (error) {
+      throw new Error(getErrorMessage(error, "createBatchRequest"));
+    }
   },
 
   async getBatchRequest(id: string) {
-    return await openai.batches.retrieve(id);
+    try {
+      return await openai.batches.retrieve(id);
+    } catch (error) {
+      throw new Error(getErrorMessage(error, "getBatchRequest"));
+    }
   },
 
   async getBatchResponseFileAsArray(file_id: string) {
-    const fileResponse = await openai.files.content(file_id);
-    const fileContents = await fileResponse.text();
-    return fileContents.split("\n");
+    try {
+      const fileResponse = await openai.files.content(file_id);
+      const fileContents = await fileResponse.text();
+      return fileContents.split("\n");
+    } catch (error) {
+      throw new Error(getErrorMessage(error, "getBatchResponseFileAsArray"));
+    }
   },
 
   async uploadJsonlFile() {
-    return await openai.files.create({
-      file: fs.createReadStream("./src/requests/requests.jsonl"),
-      purpose: "batch",
-    });
+    try {
+      return await openai.files.create({
+        file: fs.createReadStream("./src/requests/requests.jsonl"),
+        purpose: "batch",
+      });
+    } catch (error) {
+      throw new Error(getErrorMessage(error, "uploadJsonlFile"));
+    }
   },
 
   createOpenaiRequestString(html: string, job_id: number): string {
@@ -40,7 +57,7 @@ const openaiAPI = {
           {
             role: "system",
             content:
-              "You will receive a string of html from a job board. Respond with the job titles listed in the html.",
+              "You will receive a string of html from a job board. Respond with the job titles listed in the html. Format the titles as a javascript string array. Your response should start and end with brackets. For example a response could be ['Job 1', 'Job 2', 'Job 3']",
           },
           {
             role: "user",
