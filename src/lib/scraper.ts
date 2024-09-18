d170b683e1892bdd1c058d9d5bab493dce426d59
Pedro Enciso <penciso@ucsd.edu>
2024-09-17 18:05:54 -0700

    returned to original state

--- src/lib/scraper.ts
+++ src/lib/scraper.ts
@@ -1,31 +1,26 @@
-import playwright_dev from "playwright";
-import chromium from "@sparticuz/chromium";
-import playwright_prod from "playwright-core";
+import playwright from "playwright";
+import { getErrorMessage } from "./util";
 
 const scraperAPI = {
   async getHtmlFromJobPages(job_urls: string[]): Promise<string[]> {
-    // launch chrome browser
-    // const browser = await playwright_dev["chromium"].launch();
-    let browser;
-    if (process.env.ENVIRONMENT === "DEVELOPMENT") {
-      browser = await playwright_dev.chromium.launch();
-    } else {
-      browser = await playwright_prod.chromium.connect(
-        `https://production-sfo.browserless.io/firefox/playwright?token=${process.env.BROWSERLESS_API_KEY}`
-      );
+    try {
+      // launch chrome browser
+      const browser = await playwright.chromium.launch();
+      // create a new page
+      const context = await browser.newContext();
+      const page = await context.newPage();
+      // go to each url
+      const htmlArray: string[] = [];
+      for (const url of job_urls) {
+        await page.goto(url);
+        const content = await page.content();
+        // add html content to array
+        htmlArray.push(content);
+      }
+      return htmlArray;
+    } catch (error) {
+      throw new Error(getErrorMessage(error, "getHtmlFromJobPages"));
     }
-    // create a new page
-    const context = await browser.newContext();
-    const page = await context.newPage();
-    // go to each url
-    const htmlArray: string[] = [];
-    for (const url of job_urls) {
-      await page.goto(url);
-      const content = await page.content();
-      // add html content to array
-      htmlArray.push(content);
-    }
-    return htmlArray;
   },
 };
 
