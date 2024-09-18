6626e8d543745b0f65913e23ca768bd02d58e4be
Pedro Enciso <penciso@ucsd.edu>
2024-09-18 16:08:16 -0700

    dateIsTodayPST

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -6,3 +6,24 @@ export function getErrorMessage(error: any, name: string): string {
     return `Unknown error in ${name}`;
   }
 }
+
+// checks if the given date is from today in Pacific Time (Los Angeles)
+export function dateIsTodayPST(date: Date): boolean {
+  const utc_date = date.getUTCDate();
+  const utc_hour = date.getUTCHours();
+  const now = new Date();
+
+  if (utc_date === now.getUTCDate()) {
+    if (utc_hour > 6) {
+      // date is today
+      return true;
+    }
+  } else if (utc_date === now.getUTCDate() + 1) {
+    if (utc_hour < 7) {
+      // date is today
+      return true;
+    }
+  }
+
+  return false;
+}
