18b986e111da3bebfeef59d2cafc313fc059f1f8
Pedro Enciso <penciso@ucsd.edu>
2024-09-24 14:36:32 -0700

    added containsJobWithin48Hours

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,3 +1,5 @@
+import { Job } from "../types";
+
 export function getErrorMessage(error: any, name: string): string {
   if (error instanceof Error) {
     return `Error in ${name}: ${error.message}`;
@@ -39,4 +41,18 @@ export function getStartAndEndHours(): { start_date: Date; end_date: Date } {
     start_date: start,
     end_date: end,
   };
+}
+
+export function containsJobWithin48Hours(user_jobs: Job[]): Boolean {
+  const now = Date.now();
+  for (const job of user_jobs) {
+    // get the difference in time
+    const job_found_at = job.found_at.getTime();
+    const time_difference = job_found_at - now;
+    // check if the job is within 48 hours of now
+    if (time_difference < 172800) {
+      return true;
+    }
+  }
+  return false;
 }
