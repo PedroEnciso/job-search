99622fbce44b661f8391ebc0de5d25cc60fc2621
Pedro Enciso <penciso@ucsd.edu>
2024-09-30 14:04:29 -0700

    created view_router

--- src/routes/view.ts
+++ src/routes/view.ts
@@ -1,8 +1,21 @@
 import express from "express";
 import type { Request, Response } from "express";
+import { checkForUser } from "../middleware/checkForUser";
 
 export const view_router = express.Router();
 
-view_router.get("/", (req: Request, res: Response) => {
-  res.render("index");
+view_router.get("/", checkForUser, async (req: Request, res: Response) => {
+  if (req.headers["hx-target"]) {
+    res.render("companies");
+  } else {
+    res.render("index", { content: "companies" });
+  }
+});
+
+view_router.get("/login", checkForUser, async (req: Request, res: Response) => {
+  if (req.headers["hx-target"]) {
+    res.render("login");
+  } else {
+    res.render("index", { content: "login" });
+  }
 });
