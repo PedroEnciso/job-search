054c937bf6014f526d4dee4829375d0429093872
Pedro Enciso <penciso@ucsd.edu>
2024-10-07 15:10:43 -0700

    allow multiple lines of json

--- src/lib/fileWriter.ts
+++ src/lib/fileWriter.ts
@@ -1,21 +1,41 @@
-import { writeFileSync } from "fs";
+import fs_promise from "node:fs/promises";
+import fs from "fs";
 import openaiAPI from "./openai";
 import type { Company } from "../types";
 
 const path = "./src/requests/requests.jsonl";
 
 const fileWriterAPI = {
-  writeOpenaiRequestsToJsonlFIle(
+  async writeOpenaiRequestsToJsonlFIle(
     htmlArray: string[],
     companies: Company[]
-  ): void {
+  ): Promise<void> {
     for (let i = 0; i < htmlArray.length; i++) {
-      const jsonString = openaiAPI.createOpenaiRequestString(
+      const json_string = openaiAPI.createOpenaiRequestString(
         htmlArray[i],
         companies[i].id
       );
       try {
-        writeFileSync(path, jsonString);
+        // const data = await fs_promise.readFile(path, { encoding: "utf8" });
+
+        // check if the file has been created
+        let json_data_string = "";
+        if (fs.existsSync(path)) {
+          // file has been created, add to json_data_string
+          json_data_string = fs.readFileSync(path, "utf8");
+        }
+        const all_data = json_data_string.concat(json_string);
+        fs.writeFileSync(path, all_data);
+
+        // const json_data = file.fs.readFile(
+        //   "results.json",
+        //   function (err, data) {
+        //     var json = JSON.parse(data);
+        //     json.push("search result: " + currentSearchResult);
+
+        //     fs.writeFile("results.json", JSON.stringify(json));
+        //   }
+        // );
       } catch (error) {
         console.log(
           "There was an error writing the request for " +
