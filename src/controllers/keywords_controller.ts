72833e9f485cd78cfdc3493c1e42d5a03f2baa58
Pedro Enciso <penciso@ucsd.edu>
2024-10-25 11:08:54 -0700

    added post_new_keywords

--- src/controllers/keywords_controller.ts
+++ src/controllers/keywords_controller.ts
@@ -1,6 +1,11 @@
 import { Request, Response } from "express";
-import { getUserKeywords } from "../db/queries";
+import {
+  createNewKeywords,
+  createNewUserKeywords,
+  getUserKeywords,
+} from "../db/queries";
 import { Supabase_User_Request } from "../middleware/checkForUser";
+import { check } from "express-validator";
 
 async function get_keywords(req: Request, res: Response) {
   try {
@@ -43,8 +48,30 @@ async function get_new_keywords(req: Request, res: Response) {
 }
 
 async function post_new_keywords(req: Request, res: Response) {
-  console.log(req.body);
-  res.send("hell yea");
+  try {
+    const { user_id } = req.supabase_user as Supabase_User_Request;
+    // get validated & sanitized keywords as an array
+    const keywords = await validateNewKeywords(req);
+    // check if there are keywords to add
+    if (keywords.length === 0) {
+      throw new Error("No keywords are valid.");
+    }
+    // add keywords to keywords table
+    const new_keywords = await createNewKeywords(keywords);
+    // add keyword user relations to user_keywords
+    await createNewUserKeywords(
+      new_keywords.map((key) => key.id),
+      user_id
+    );
+    res.redirect("/keywords");
+  } catch (error) {
+    res.render("index", {
+      page: "Keywords",
+      content: "new keywords",
+      error,
+      inputs: 1,
+    });
+  }
 }
 
 export default {
@@ -52,3 +79,14 @@ export default {
   get_new_keywords,
   post_new_keywords,
 };
+
+async function validateNewKeywords(req: Request) {
+  const valid_keywords: string[] = [];
+  for (let key in req.body) {
+    const result = await check(key).trim().notEmpty().escape().run(req);
+    if (result.isEmpty()) {
+      valid_keywords.push(req.body[key]);
+    }
+  }
+  return valid_keywords;
+}
