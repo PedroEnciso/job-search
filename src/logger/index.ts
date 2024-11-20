8d6bc3024f48a4bafcf150b4a1889c3df96b08fc
Pedro Enciso <penciso@ucsd.edu>
2024-11-20 10:22:42 -0800

    winston logs to console

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -5,6 +5,7 @@ export const logger = winston.createLogger({
   level: "info",
   format: combine(timestamp(), json()),
   transports: [
+    new winston.transports.Console(),
     new winston.transports.File({
       filename: "combined.log",
     }),
