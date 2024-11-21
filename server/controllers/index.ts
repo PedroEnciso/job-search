c14a19627ca3a06a4f1d10e7704da3bd3d96547e
Pedro Enciso <penciso@ucsd.edu>
2024-11-21 13:00:06 -0800

    created api controllers

--- server/controllers/index.ts
+++ server/controllers/index.ts
@@ -1,4 +1,8 @@
 export { default as auth_controller } from "./auth_controller";
-export { default as current_openings_controller } from "./current_openings_controller";
-export { default as companies_controller } from "./companies_controller";
-export { default as keywords_controller } from "./keywords_controller";
+export { default as current_openings_controller_deprecated } from "./current_openings_controller";
+export { default as companies_controller_deprecated } from "./companies_controller";
+export { default as keywords_controller_deprecated } from "./keywords_controller";
+
+export { default as current_jobs_controller } from "./Current_Jobs";
+export { default as companies_controller } from "./Companies";
+export { default as keywords_controller } from "./Keywords";
--- server/routes/view.ts
+++ server/routes/view.ts
@@ -2,9 +2,9 @@ import express from "express";
 import { checkForUser } from "../middleware/checkForUser";
 import {
   auth_controller,
-  current_openings_controller,
-  companies_controller,
-  keywords_controller,
+  current_openings_controller_deprecated,
+  companies_controller_deprecated,
+  keywords_controller_deprecated,
 } from "../controllers";
 
 export const view_router = express.Router();
@@ -13,46 +13,54 @@ export const view_router = express.Router();
 view_router.get(
   "/",
   checkForUser,
-  current_openings_controller.get_current_openings
+  current_openings_controller_deprecated.get_current_openings
 );
 // companies
-view_router.get("/companies", checkForUser, companies_controller.get_companies);
+view_router.get(
+  "/companies",
+  checkForUser,
+  companies_controller_deprecated.get_companies
+);
 view_router.get(
   "/companies/new",
   checkForUser,
-  companies_controller.get_new_company
+  companies_controller_deprecated.get_new_company
 );
 view_router.patch(
   "/companies/:id",
   checkForUser,
-  companies_controller.patch_company
+  companies_controller_deprecated.patch_company
 );
 view_router.delete(
   "/companies/:id",
   checkForUser,
-  companies_controller.delete_company
+  companies_controller_deprecated.delete_company
 );
 view_router.post(
   "/companies/new",
   checkForUser,
-  companies_controller.post_new_company
+  companies_controller_deprecated.post_new_company
 );
 // keywords
-view_router.get("/keywords", checkForUser, keywords_controller.get_keywords);
+view_router.get(
+  "/keywords",
+  checkForUser,
+  keywords_controller_deprecated.get_keywords
+);
 view_router.get(
   "/keywords/new",
   checkForUser,
-  keywords_controller.get_new_keywords
+  keywords_controller_deprecated.get_new_keywords
 );
 view_router.post(
   "/keywords/new",
   checkForUser,
-  keywords_controller.post_new_keywords
+  keywords_controller_deprecated.post_new_keywords
 );
 view_router.delete(
   "/keywords/:id",
   checkForUser,
-  keywords_controller.delete_keyword
+  keywords_controller_deprecated.delete_keyword
 );
 //// AUTH ROUTES
 // login
