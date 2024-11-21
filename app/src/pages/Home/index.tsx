f9113b3ac687579e1b58040fedcd093b4f5bfbdd
Pedro Enciso <penciso@ucsd.edu>
2024-11-21 13:38:42 -0800

    fetch current jobs

--- app/src/pages/Home/index.tsx
+++ app/src/pages/Home/index.tsx
@@ -1,7 +1,15 @@
-import { useGetCompaniesQuery } from "@/lib/queries";
+import { useGetCompaniesQuery, useGetCurrentJobsQuery } from "@/lib/queries";
 import CompaniesSection from "@/components/Companies";
+import CurrentJobsSection from "@/components/CurrentJobs";
 
 function Home() {
+  // fetch current jobs
+  const {
+    isPending: current_jobs_is_pending,
+    error: current_jobs_error,
+    data: current_jobs,
+  } = useGetCurrentJobsQuery();
+  // fetch companies
   const {
     isPending: company_is_pending,
     error: company_error,
@@ -11,6 +19,11 @@ function Home() {
   return (
     <div>
       <h1>JobScout</h1>
+      <CurrentJobsSection
+        is_pending={current_jobs_is_pending}
+        error={current_jobs_error}
+        current_jobs={current_jobs}
+      />
       <CompaniesSection
         is_pending={company_is_pending}
         error={company_error}
