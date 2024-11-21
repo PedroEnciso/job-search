000f3e32aa673f474beab07aa9ced4a13f3e76b6
Pedro Enciso <penciso@ucsd.edu>
2024-11-21 13:38:22 -0800

    added useGetCurrentJobsQuery

--- app/src/lib/queries.ts
+++ app/src/lib/queries.ts
@@ -1,10 +1,23 @@
 import { useQuery } from "@tanstack/react-query";
-import type { CompanyWithIsActive } from "../../../server/types";
+import type {
+  CompanyWithIsActive,
+  FrontendCurrentJob,
+} from "../../../server/types";
 
 export function useGetCompaniesQuery() {
   return useQuery({
     queryKey: ["companies"],
     queryFn: (): Promise<CompanyWithIsActive[]> =>
       fetch("http://localhost:4321/api/v1/companies").then((res) => res.json()),
+  });
+}
+
+export function useGetCurrentJobsQuery() {
+  return useQuery({
+    queryKey: ["current_jobs"],
+    queryFn: (): Promise<FrontendCurrentJob[]> =>
+      fetch("http://localhost:4321/api/v1/current_jobs").then((res) =>
+        res.json()
+      ),
   });
 }
