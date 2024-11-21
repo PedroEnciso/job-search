8d4610970baf3a3cef517bb796d6690d57d38d3b
Pedro Enciso <penciso@ucsd.edu>
2024-11-21 11:47:54 -0800

    moved companies to dedicated component

--- app/src/pages/Home/index.tsx
+++ app/src/pages/Home/index.tsx
@@ -1,45 +1,23 @@
-import { useEffect, useState } from "react";
-import type { CompanyWithIsActive } from "../../../../server/types";
+import { useGetCompaniesQuery } from "@/lib/queries";
+import CompaniesSection from "@/components/Companies";
 
 function Home() {
-  const [companies, setCompanies] = useState<CompanyState>(null);
-  const [error, setError] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const {
+    isPending: company_is_pending,
+    error: company_error,
+    data: company_data,
+  } = useGetCompaniesQuery();
 
-  useEffect(() => {
-    async function getCompanies() {
-      setIsLoading(true);
-      const response = await fetch("http://localhost:4321/api/v1/companies");
-      if (!response.ok) {
-        console.log(response);
-        const error = (await response.json()) as Error;
-        setIsLoading(false);
-        setError(error.message);
-        return;
-      }
-      const data = await response.json();
-      console.log(data);
-      setIsLoading(false);
-      setCompanies(data);
-    }
-    getCompanies();
-  }, []);
   return (
     <div>
       <h1>JobScout</h1>
-      {isLoading ? <p>Loading companies...</p> : null}
-      {error ? <p>{error}</p> : null}
-      {companies ? (
-        <ul>
-          {companies.map((company) => (
-            <li key={company.id}>{company.name}</li>
-          ))}
-        </ul>
-      ) : null}
+      <CompaniesSection
+        is_pending={company_is_pending}
+        error={company_error}
+        companies={company_data}
+      />
     </div>
   );
 }
 
 export default Home;
-
-type CompanyState = CompanyWithIsActive[] | null;
